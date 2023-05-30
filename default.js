2ddda8df98cafb687a09f71926c314cbc7ae2e2b
Younggu Kang <khkkor3@naver.com>
2023-05-30 13:23:25 +0900

    5.30 문자열 연결하기

--- default.js
+++ default.js
@@ -1,46 +1,57 @@
-document.write('Hello World');
-document.write("<h1>Welcome to JS Program</h1>")
-document.write("<h2>Welcome to JS Program</h2>")
+//document.write('Hello World');
+//document.write("<h1>Welcome to JS Program</h1>")
+//document.write("<h2>Welcome to JS Program</h2>")
+//
+//console.log('Welcome JS Program');
+//console.info('Welcome JS Program');
+//console.warn('Welcome JS Program');
+//console.error('Welcome JS Program');
+//
+//alert('Welcome JS Program');
+//var a = prompt('Welcome JS Program');
+//console.log(a);
+//
+//console.log(typeof 123);
+//console.log(typeof 123.5);
+//console.log(typeof "123");
+//console.log(typeof true);
+//console.log(typeof false);
+//
+//var car;
+//console.log(car);
+//var car = "";
+//console.log(typeof car);
+//
+//var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
+//console.log(typeof person, person);
+//person = null;
+//console.log(typeof person, person);
+//
+//
+//var name = "이승훈";
+//var age = 29;
+//var cgpa = 3.92;
+//var lineBreak = "<br/>"
+//
+//document.write("이름: " + name + lineBreak);
+//document.write("나이: " + age + lineBreak);
+//document.write("학점: " + cgpa + lineBreak);
 
-console.log('Welcome JS Program');
-console.info('Welcome JS Program');
-console.warn('Welcome JS Program');
-console.error('Welcome JS Program');
+var lName = "홍";
+var fName = "길동";
 
-alert('Welcome JS Program');
-var a = prompt('Welcome JS Program');
-console.log(a);
+var fullName = lName + fName;
 
-console.log(typeof 123);
-console.log(typeof 123.5);
-console.log(typeof "123");
-console.log(typeof true);
-console.log(typeof false);
+console.log(fullName);
+console.log("Today is" + "a" + "beautiful day");
+console.log("My name is" + fullName);
 
-var car;
-console.log(car);
-var car = "";
-console.log(typeof car);
-
-var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
-console.log(typeof person, person);
-person = null;
-console.log(typeof person, person);
-
-
-var name = "이승훈";
-var age = 29;
-var cgpa = 3.92;
-var lineBreak = "<br/>"
-
-document.write("이름: " + name + lineBreak);
-document.write("나이: " + age + lineBreak);
-document.write("학점: " + cgpa + lineBreak);
-
-
-
-
-
+var num1 = 20;
+var num2 = 30;
+var sum = num1 + num2;
+console.log(num1 + num2);
+console.log("" + num1 + num2);
+console.log(num1 + " + " + num2 + " = " + sum);
 
 
 
