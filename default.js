90a77340ed54a2bd8f7459341dbbe7b6a4c16f15
Younggu Kang <khkkor3@naver.com>
2023-05-30 11:23:01 +0900

    5.30 출력함수 실습

--- default.js
+++ default.js
@@ -1,1 +1,12 @@
 document.write('Hello World');
+document.write("<h1>Welcome to JS Program</h1>")
+document.write("<h2>Welcome to JS Program</h2>")
+
+console.log('Welcome JS Program');
+console.info('Welcome JS Program');
+console.warn('Welcome JS Program');
+console.error('Welcome JS Program');
+
+alert('Welcome JS Program');
+var a = prompt('Welcome JS Program');
+console.log(a);
