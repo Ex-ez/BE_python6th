f120451569bec72478c7068ec290a807d840e125
Younggu Kang <khkkor3@naver.com>
2023-05-31 09:48:19 +0900

    5.31 조건문 제어문

--- default.js
+++ default.js
@@ -131,35 +131,61 @@
 //
 //document.write("화씨 : " + farn);
 //
-var num1 = 20;
-var num2 = 10;
-var num3 = "10";
-var num4 = 20;
-var num5 = 15;
+//var num1 = 20;
+//var num2 = 10;
+//var num3 = "10";
+//var num4 = 20;
+//var num5 = 15;
+//
+//console.log('일반 크기비교');
+//console.log(num1 > num2, num1, '>', num2);
+//console.log(num1 >= num2, num1, '>=', num2);
+//console.log(num1 < num2, num1, '<', num2);
+//console.log(num1 <= num2, num1, '<=', num2);
+//
+//console.log('같은지 여부 확인')
+//console.log(num1 == num4, num1, '==', num4)
+//console.log(num1 != num4, num1, '!=', num4)
+//
+//console.log('===')
+//console.log(num1 === num3, num1, '===', num3)
+//console.log(num2 === num3, num2, '===', num3);
+//console.log(num2 == num3, num2, '==', num3);
+//console.log(num1 !== num3);
+//
+//console.log('num1 > num2 && num1 > num5: ', num1 > num2 && !(num1 > num5));
+//console.log('num1 > num2 || num1 > num5: ', num1 > num2 || num1 > num5);
 
-console.log('일반 크기비교');
-console.log(num1 > num2, num1, '>', num2);
-console.log(num1 >= num2, num1, '>=', num2);
-console.log(num1 < num2, num1, '<', num2);
-console.log(num1 <= num2, num1, '<=', num2);
+var num1 = parseInt(prompt("첫번째 숫자 입력 : "));
+var num2 = parseInt(prompt("두번째 숫자 입력  : "));
 
-console.log('같은지 여부 확인')
-console.log(num1 == num4, num1, '==', num4)
-console.log(num1 != num4, num1, '!=', num4)
+if(num1 > num2) {
+    console.log("큰 수는 num1 : " + num1);
+}
 
-console.log('===')
-console.log(num1 === num3, num1, '===', num3)
-console.log(num2 === num3, num2, '===', num3);
-console.log(num2 == num3, num2, '==', num3);
-console.log(num1 !== num3);
+if(num1 < num2) {
+    console.log("큰 수는 num2 : " + num2);
+}
 
-console.log('num1 > num2 && num1 > num5: ', num1 > num2 && !(num1 > num5));
-console.log('num1 > num2 || num1 > num5: ', num1 > num2 || num1 > num5);
+if(num1 == num2) {
+    console.log("같은 수");
+}
 
+if(num1 > num2) {
+    console.log("큰 수 num1:" + num1);
+} else if (num1 < num2) {
+    console.log("큰 수 num2:" + num2);
+} else if (num1 == num2) {
+    console.log("같은 수");
+}
 
-
-
-
+if(num1 > num2) {
+    console.log("큰 수 num1:" + num1);
+} else if (num1 < num2) {
+    console.log("큰 수 num2:" + num2);
+} else (num1 == num2) {
+    console.log("같은 수");
+}
 
 
 
