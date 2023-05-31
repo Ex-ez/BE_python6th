c7588ef55523123440e50704332cd71b75d0db4a
Younggu Kang <khkkor3@naver.com>
2023-05-31 09:37:59 +0900

    5.31 관계, 논리 연산자

--- default.js
+++ default.js
@@ -126,13 +126,35 @@
 //
 //document.write("Area of triangle = " + area);
 //
-var cels = parseFloat(prompt("섭씨 입력: "));
-var farn = cels * (9 / 5) + 32;
+//var cels = parseFloat(prompt("섭씨 입력: "));
+//var farn = cels * (9 / 5) + 32;
+//
+//document.write("화씨 : " + farn);
+//
+var num1 = 20;
+var num2 = 10;
+var num3 = "10";
+var num4 = 20;
+var num5 = 15;
 
-document.write("화씨 : " + farn);
+console.log('일반 크기비교');
+console.log(num1 > num2, num1, '>', num2);
+console.log(num1 >= num2, num1, '>=', num2);
+console.log(num1 < num2, num1, '<', num2);
+console.log(num1 <= num2, num1, '<=', num2);
 
+console.log('같은지 여부 확인')
+console.log(num1 == num4, num1, '==', num4)
+console.log(num1 != num4, num1, '!=', num4)
 
+console.log('===')
+console.log(num1 === num3, num1, '===', num3)
+console.log(num2 === num3, num2, '===', num3);
+console.log(num2 == num3, num2, '==', num3);
+console.log(num1 !== num3);
 
+console.log('num1 > num2 && num1 > num5: ', num1 > num2 && !(num1 > num5));
+console.log('num1 > num2 || num1 > num5: ', num1 > num2 || num1 > num5);
 
 
 
