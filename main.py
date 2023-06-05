d5d04eb1dd422e6d9505f2814bd2e0fb08ca6107
Younggu Kang <khkkor3@naver.com>
2023-06-05 14:14:38 +0900

    6.5 암시적 타입변환

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-# is 연산자
+# 암시적 타입 변환
 
-a = 10
-b = 10
-print(a is b)
+a = 5
+b = 2
+print(b, type(b))
+value = a / b
+print(value)
+print(type(value))
 
-a = 10
-b = '10'
-print(a is b)
+x = 10
+y = 5.5
+total = x + y
+print(total)
+print(type(total))
 
-a = 10
-b = 10
-print(a is not b)
+j = "Hello"
+k = "like lion"
+p = j + k
+print(p)
+print(type(p))
 
-a = 10
-b = '10'
-print(a is not b)
+q = 20
+u = '10'
+r = p + u
+print(r)
