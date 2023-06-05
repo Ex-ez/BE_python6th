e19375c7ff56672e8820fbed219ee2fe3628c699
Younggu Kang <khkkor3@naver.com>
2023-06-05 13:11:03 +0900

    6.5 비교 연산자 실습

--- main.py
+++ main.py
@@ -1,47 +1,24 @@
-# 덧셈
-a = 4
-b = 2
-total = a + b
-print('total = a + b:', total)
-
-# 뺄셈
-a = 4
-b = 2
-total = a - b
-print('total = a - b:', total)
-
-# 곱셈
-a = 6
-b = 3
-total = a * b
-print('total = a * b:', total)
-
-# 나눗셈
-a = 4
-b = 2
-total = a / b
-print('total = a / b:', total)
-
-# 나머지
+# 비교 연산자
 a = 5
 b = 2
-total = a % b
-print('total = a % b:', total)
+print('a < b: ', a < b)
 
-# 거듭제곱
 a = 5
 b = 2
-total = a**b
-print('total = a ** b:', total)
+print('a > b: ', a > b)
 
-# 몫(양수)
 a = 5
 b = 2
-total = a//b
-print('total = a // b:', total)
+print('a <= b: ', a <= b)
 
-# 몫(음수)
-a = -5
+a = 5
 b = 2
-total = a // b
-print('total = a + b:', total)
+print('a >= b: ', a >= b)
+
+a = 5
+b = 2
+print('a == b: ', a == b)
+
+a = 5
+b = 2
+print('a != b: ', a != b)
