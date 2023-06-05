5b480da4fdb71769356f6819e28c3a23d5c00604
Younggu Kang <khkkor3@naver.com>
2023-06-05 13:19:38 +0900

    6.5 논리 연산자

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-# 비교 연산자
+# 논리 연산자
 a = 5
 b = 2
-print('a < b: ', a < b)
+c = 3
+d = 200
 
-a = 5
-b = 2
-print('a > b: ', a > b)
+print('AND 연산자')
+print('a > b and a < c: ', a > b and a < c)
 
-a = 5
-b = 2
-print('a <= b: ', a <= b)
+print('OR 연산자')
+print('a > b or d a < c: ', a > b or a < c)
 
-a = 5
-b = 2
-print('a >= b: ', a >= b)
-
-a = 5
-b = 2
-print('a == b: ', a == b)
-
-a = 5
-b = 2
-print('a != b: ', a != b)
+print('NOT 연산자')
+print('no(a < b): ', not(a < b))
