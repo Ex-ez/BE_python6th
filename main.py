e3b4abf60693481abab6644b6e7839794f300a0b
Younggu Kang <khkkor3@naver.com>
2023-06-05 10:22:58 +0900

    6.5 파이썬 실습

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+#변수 선언
+a = 10
+b = 3.14
+c = "Hello, World!"
+d = [1, 2, 3]
+e = (4, 5, 6)
+f = {7, 8, 9}
+g = {"apple": 1, "banan": 2, "orange": 3}
+
+# 데이터 타입 출력
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
