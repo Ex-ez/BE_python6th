c1544b5556df9a04abb07781b30a2d2c2f050f92
Younggu Kang <khkkor3@naver.com>
2023-06-05 14:18:04 +0900

    6.5 부동 소숫점 결과를 정수로 변환

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-# 암시적 타입 변환
+# 명시적 타입 변환
 
 a = 5
 b = 2
-print(b, type(b))
 value = a / b
-print(value)
 print(type(value))
-
-x = 10
-y = 5.5
-total = x + y
-print(total)
-print(type(total))
-
-j = "Hello"
-k = "like lion"
-p = j + k
-print(p)
-print(type(p))
-
-q = 20
-u = '10'
-r = p + u
-print(r)
+int_value = int(value)
+print(int_value, type(int_value))
