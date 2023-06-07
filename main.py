be1c4f9ab644e9e74a4c7d8978b5bbae0c4293a5
Younggu Kang <khkkor3@naver.com>
2023-06-07 14:21:56 +0900

    6.7 문자열 함수 실습

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-str1 = 'Likelion'
-str2 = "LikeLion"
-str3 = '''
-동해물과 두산이
-마르고 닳도록
-하느님이 보우하사
-우리나라 만세
-'''
-str4 = """
-동해물과 두산이
-마르고 닳도록
-하느님이 보우하사
-우리나라 만세
-"""
+s = "Hello World"
 
-print(str1)
-print(str2)
-print(str3)
-print(str4)
+print(s.upper())
 
-str5 = 'Hello "Like Lion" How are you'
-str6 = "Hello 'Like Lion' How are you"
+print(s.lower())
 
-print(str5)
-print(str6)
+print(s.swapcase())
 
-str7 = "Hello \nHow are you?"
-str8 = "Hello \\nHow are you?"
-str9 = r"Hello \nHow are you?"
+print(s.title())
 
-print(str7)
-print(str8)
-print(str9)
+print(s.isupper())
+
+print(s.islower())
+
+print(s.istitle())
+
+print(s.isdigit())
+
+print(s.isalpha())
