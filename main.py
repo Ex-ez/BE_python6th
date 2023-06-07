99ff66d711be0c12c15d79221e5e5ba5ceb9efbf
Younggu Kang <khkkor3@naver.com>
2023-06-07 14:24:43 +0900

    6.7 strip 실습

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-s = "Hello World"
+s = "    Hello World "
 
-print(s.upper())
-
-print(s.lower())
-
-print(s.swapcase())
-
-print(s.title())
-
-print(s.isupper())
-
-print(s.islower())
-
-print(s.istitle())
-
-print(s.isdigit())
-
-print(s.isalpha())
+print(s.lstrip())
+print(s.rstrip())
+print(s.strip())
