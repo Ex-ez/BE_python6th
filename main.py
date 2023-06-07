d839a168cee278dd4701da0b669424ccaa095956
Younggu Kang <khkkor3@naver.com>
2023-06-07 11:34:06 +0900

    6.7 배열의 삽입

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-# 배열 생성 및 원소 접근
-import array
-stu_roll = array.array('i', [101, 102, 103, 104, 105])
-print(stu_roll[0])
-print(stu_roll[1])
-print(stu_roll[2])
-print(stu_roll[3])
-print(stu_roll[4])
+from array import *
 
-print("for in 사용")
-for element in stu_roll:
-    print(element)
-
-print("인덱스를 이용한 순회")
+stu_roll = array('i', [101, 102, 103, 104, 105])
 n = len(stu_roll)
-for i in range(n):
-    print(i, "=", stu_roll[i])
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
 
-print("인덱스를 사용한 while 루프 배열 순회")
+print("Array After Insert")
+stu_roll.insert(1, 106)
+stu_roll.insert(3, 107)
+n = len(stu_roll)
 i = 0
 while i < n:
     print(stu_roll[i])
