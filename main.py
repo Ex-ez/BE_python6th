893ab49e84f9bdcbd5287d9cbf75d18b5603d6d3
Younggu Kang <khkkor3@naver.com>
2023-06-07 13:05:47 +0900

    6.7 배열의 index() extend() 실습

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from array import *
 
 stu_roll = array('i', [101, 102, 103, 104, 105])
 
-# print("배열 삽입 실습")
-# stu_roll.insert(1, 106)
-# stu_roll.insert(3, 107)
-# n = len(stu_roll)
-# i = 0
-# while i < n:
-#     print(stu_roll[i])
-#     i += 1
-#
-# print("배열 요소 삭제")
-#
-# stu_roll.remove(107)
+print("배열 삽입 실습")
+stu_roll.insert(1, 106)
+stu_roll.insert(3, 107)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+print("배열 요소 삭제")
+
+stu_roll.remove(107)
 n = len(stu_roll)
 i = 0
 while i < n:
@@ -29,3 +29,23 @@ i = 0
 while i < n:
     print(stu_roll[i])
     i += 1
+
+print("array index 메소드")
+print(stu_roll.index(101))
+
+print("extend() 메소드")
+arr = array('i', [201, 208, 209])
+stu_roll.extend(arr)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+print("reverse() 메소드")
+stu_roll.reverse()
+n = len(stu_roll)
+i = 0
+while i < n:
+        print(stu_roll[i])
+        i += 1
