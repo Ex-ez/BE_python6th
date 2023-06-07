88d6c9a8f86e9cbe1a7075d5c2af78d01606692e
Younggu Kang <khkkor3@naver.com>
2023-06-07 11:41:14 +0900

    6.7 배열의 샂게와 pop() 실습

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from array import *
 
 stu_roll = array('i', [101, 102, 103, 104, 105])
+
+# print("배열 삽입 실습")
+# stu_roll.insert(1, 106)
+# stu_roll.insert(3, 107)
+# n = len(stu_roll)
+# i = 0
+# while i < n:
+#     print(stu_roll[i])
+#     i += 1
+#
+# print("배열 요소 삭제")
+#
+# stu_roll.remove(107)
 n = len(stu_roll)
 i = 0
 while i < n:
     print(stu_roll[i])
     i += 1
 
-print("Array After Insert")
-stu_roll.insert(1, 106)
-stu_roll.insert(3, 107)
+print("배열 pop() 함수 실습")
+
+element = stu_roll.pop(2)
+print('element:', element)
 n = len(stu_roll)
 i = 0
 while i < n:
