67bc6ad2ef68f06e1a333b408b4dab703d460ce8
Younggu Kang <khkkor3@naver.com>
2023-06-08 15:26:25 +0900

    6.8 Call by Reference 참조를 이용한 함수 호출 실습

--- main.py
+++ main.py
@@ -1,53 +1,10 @@
-stu = {101: 'Kim', 102: 'Bae', 103: 'Hong'}
-fees = {'kim': 2000, 'bae': 3000, 'hong': 8000}
-print(stu[101])
-print(stu[102])
-print(stu[103])
+def val(lst):
+    print("Inside Function Before Append:", lst, id(lst))
+    lst.append(4)
+    print("Inside Function After Append:", lst, id(lst))
 
-print(fees['kim'])
-print(fees['bae'])
-print(fees['hong'])
 
-stu[102] = 'Python'
-
-print(stu)
-
-stu[104] = '멋쟁이사자'
-
-print(stu)
-
-del stu[102]
-
-print(stu)
-
-print(102 not in stu)
-
-# stu.clear()
-
-print(stu)
-
-new_stu = stu.copy()
-
-key = (101, 102, 103)
-value = '멋쟁이사자'
-new_stu = dict.fromkeys(key, value)
-
-print(new_stu)
-print(stu[101])
-print(stu.get(101))
-print(stu.items())
-print(stu.keys())
-print(stu.values())
-stu[104] = '멋쟁이사자'
-print(stu)
-stu.update({104: '멋쟁이사자2'})
-print(stu)
-stu.pop(104)
-print(stu)
-stu.pop(104, 'No Value')
-stu.setdefault(104, 'Park')
-print(stu)
-
-print(stu.popitem())
-print(stu.popitem())
-print(stu.popitem())
+list = [1, 2, 3]
+print("Before Calling Function:", list, id(list))
+val(list)
+print("After Calling Function:", list, id(list))
