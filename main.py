937d8a2c73c35d92f8cafbe18ef74b6eb702670d
Younggu Kang <khkkor3@naver.com>
2023-06-08 10:39:28 +0900

    6.8 지역 변수와 전역 변수 실습

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def add(x, **num):
-    z = x + num['a'] + num['b'] + num['c']
-    print('Addition: ', z)
+def show():
+    x = 10
+    print(x)
 
 
-add(3, a=5, b=2, c=4, d=5)
+show()
+
+
+def add(y):
+    x = 10
+    print(x)
+    print(x + y)
+
+
+add(20)
+
+a = 50
+
+
+def show():
+    x = 10
+    print(x)  # local
+    print(a)  # global
+
+show()
+
+print("Global Variable a:", a)
+i = 0
+def myfun():
+    a = i + 1
+    print("My Function", a)
+myfun()
+print("Global Variable a:", a)
