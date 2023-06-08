06dbca57b099de6390393d21201b289fa284d6dc
Younggu Kang <khkkor3@naver.com>
2023-06-08 13:27:08 +0900

    6.8 제너레이터 실습 2

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+def generate_alphabet(start_letter, end_letter):
+    start = ord(start_letter)
+    end = ord(end_letter)
+    while start <= end:
+        yield chr(start)
+        start += 1
 
 
-runner = fibonacci(100)
+runner = generate_alphabet('A', 'F')
 
-print(next(runner))
-
-print("======")
-print(runner)
-print(next(runner))
-print("======")
-
-for num in runner:
-    print(num)
+for letter in runner:
+    print(letter)
