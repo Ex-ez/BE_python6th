16a5e18a77fe3fe10bc6a1c87e4644104a2ad71f
Younggu Kang <khkkor3@naver.com>
2023-06-08 15:39:06 +0900

    6.8 재귀호출

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def val(x):
-    print("Inside :", x, id(x))
-    x += 1
-    print("Inside After:", x, id(x))
+import sys
+print("default:", sys.getrecursionlimit())
+sys.setrecursionlimit(3000)
+print("setting:", sys.getrecursionlimit())
+i = 0
+def myfun():
+    global i
+    i += 1
+    print("My Function:", i)
+    myfun()
 
-print("====")
-x = 10
-print("Before Calling:", x, id(x))
-val(x)
-print("After Calling: ", x, id(x))
+myfun()
