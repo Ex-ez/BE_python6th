afaae6ec0cf63ec19fcfb809741762bae87e850d
Younggu Kang <khkkor3@naver.com>
2023-06-08 15:44:34 +0900

    6.8 람다 익명 함수

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import sys
-print("default:", sys.getrecursionlimit())
-sys.setrecursionlimit(3000)
-print("setting:", sys.getrecursionlimit())
-i = 0
-def myfun():
-    global i
-    i += 1
-    print("My Function:", i)
-    myfun()
+show = lambda x: print(x)
 
-myfun()
+show(5)
+
+add = lambda x, y: (x + y)
+
+print(add(5, 2))
+
+add_sub = lambda x, y: (x + y, x - y)
+a, s = add_sub(5, 2)
+
+print(a)
+print(s)
+
+add = lambda x, y=3: (x + y)
+
+print(add(5))
