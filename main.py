0fa2c464927ac5c38b2a78dda884bdf9e982ab31
Younggu Kang <khkkor3@naver.com>
2023-06-08 14:12:34 +0900

    6.8 기본 set 실습

--- main.py
+++ main.py
@@ -1,40 +1,17 @@
-b = (10)
-c = (10,)
+a = {10, 20, 30}
+a = {10, 20, 30, "멋쟁이사자", "Kang", 40}
+a = {10, 20, 30, "멋쟁이사자", "Kang", 40, 10, 20}
 
-print(b)
-print(c)
+b = set()
+print(type(b))
+a.add(50)
+a.update([10, 20, 60, 70])
+print(a)
+a.remove('멋쟁이사자')
+a.discard('멋쟁이사자')
+a.discard(70)
+print(a)
 
-d = (10, 20, 30, 40)
-e = (10, 20, -50, 21.3, '멋쟁이사자')
-f = 10, 20, -50, 21.3, '멋쟁이사자'
-
-print(d, e, f,sep="\n")
-
-print(f[1])
-print(f[2])
-print(f[3])
-print(f[4])
-print(f[:3])
-print(f[1:4])
-print(f[3:])
-g = c + f
-print(g)
-print(f * 5)
-print(-10 in f)
-print("===========")
-h = (10, 20, -50, 20)
-print(min(h), max(h))
-print(h.count(20))
-print(h.index(20))
-sorted_a = sorted(h)
-print(sorted_a)
-a = (10, 20, -50)
-x, y, z = a
-print(x, y, z)
-
-a = 1
-b = 2
-print(a, b)
-
-a, b = b, a
-print(a, b)
+new_set = a.copy()
+new_set.clear()
+print(new_set)
