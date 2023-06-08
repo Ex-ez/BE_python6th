a387f08709153e42a5212bcfbcbf8d0017619026
Younggu Kang <khkkor3@naver.com>
2023-06-08 15:30:58 +0900

    6.8 Call by Value (예시)

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def val(lst):
-    print("Inside Function Before Append:", lst, id(lst))
-    lst.append(4)
-    print("Inside Function After Append:", lst, id(lst))
+def val(x):
+    print("Inside :", x, id(x))
+    x += 1
+    print("Inside After:", x, id(x))
 
-
-list = [1, 2, 3]
-print("Before Calling Function:", list, id(list))
-val(list)
-print("After Calling Function:", list, id(list))
+print("====")
+x = 10
+print("Before Calling:", x, id(x))
+val(x)
+print("After Calling: ", x, id(x))
