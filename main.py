261e756d4e44f35638a233d49f109cb65a53c285
Younggu Kang <khkkor3@naver.com>
2023-06-08 15:52:53 +0900

    6.8 배열 전달 및 반환 실습

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-def decor(fun):
-    def inner():
-        a = fun()
-        add = a + 5
-        return add
-
-    return inner
+from array import *
 
 
-def num():
-    return 10
+def show(ar):
+    print("Passed Array:", ar)
+    print(type(ar))
+    for i in ar:
+        print(i)
+    return ar
 
 
-result_fun = decor(num)
-print(result_fun())
-
-
-@decor
-def num():
-    return 10
-
-
-print(num())
+print("====")
+a = array('i', [101, 102, 103, 104])
+y = show(a)
+print("Return Array y:", y)
+print(type(y))
+for i in y:
+    print(i)
