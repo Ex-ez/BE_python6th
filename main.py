e14f1af2988de56b2289aeabfd819df85dd16788
Younggu Kang <khkkor3@naver.com>
2023-06-09 14:16:35 +0900

    6.9 3파일 위치 확인 및 변경

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-with open('example.txt', 'r') as file_object:
-    lines = file_object.readlines()
-    for line in lines:
-        print('>', line.strip())
+print("파일 열기")
+file_object = open('example.txt', 'r')
 
+print("현재 파일 위치 확인")
+position = file_object.tell()
+print("Current position: ", position)
+
+print("파일 포인터 위치 변경")
+file_object.seek(7)
+
+print("변경된 위치 확인")
+position = file_object.tell()
+print("New position:", position)
+
+print("파일 닫기")
+file_object.close()
