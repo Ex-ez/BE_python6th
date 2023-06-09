8f14c8202bd5fb81cbe34722ae2c8da4b0872f81
Younggu Kang <khkkor3@naver.com>
2023-06-09 13:25:02 +0900

    6.9 실습 파일 열고 닫기

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-from datetime import timedelta
-from datetime import date
-from datetime import datetime
+file_object = open('example.txt', 'r')
 
-td = timedelta(days=10)
-print(td)
+content = file_object.read()
 
-d1 = date(year=2023, month= 5, day= 5)
-d2 = date(year=2023, month= 6, day= 9)
+print(content)
 
-# 날짜의 연산자 오버로딩으로 비요할 수 있습니다.
-print(d1 == d2)
-print(d1 < d2)
-print(d1 > d2)
-
-dt = datetime.today()
-
-formatted_datetime = dt.strftime('%B, %d, %y')
-print(formatted_datetime)
+file_object.close()
