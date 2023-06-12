efb640f7ac06811a838ca44b1c4646c714b59f1c
Younggu Kang <khkkor3@naver.com>
2023-06-12 14:22:32 +0900

    6.12 enumerate

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-class CustomException(Exception):
-    def __init__(self, message):
-        self.message = message
+a = ['a1', 'b2', 'c3']
 
+for i in range(len(a)):
+    print(i, a[i])
 
-try:
-    raise CustomException("This is a custom exception.")
-except CustomException as e:
-    print(f"Error: {e.message}")
+i = 0
+for v in a:
+    print(i, v)
+    i += 1
+
+for i, v in enumerate(a):
+    print(i, v)
