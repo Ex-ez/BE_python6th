8c0b6c38d1799a8fb471e9e2317dbdc309c3b1ca
Younggu Kang <khkkor3@naver.com>
2023-06-12 13:18:25 +0900

    6.12 custom exception

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+class CustomException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
 try:
-    # 잘못된 입력
-    number = int("Not a number")
-    # number = 5 + "Not a number"
-except ValueError:
-    print("Error: Invalid value.")
-except TypeError:
-    print("Error: Invalid type.")
+    raise CustomException("This is a custom exception.")
+except CustomException as e:
+    print(f"Error: {e.message}")
