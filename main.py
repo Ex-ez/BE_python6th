ddd8cd691be9bb8314adc9bc388204ea771f6f5e
Younggu Kang <khkkor3@naver.com>
2023-06-12 13:11:42 +0900

    6.12 여러 예외 처리
    다양한 try-except 구문

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 try:
-    result = 10 / 0
-except ZeroDivisionError:
-    print("Error: Division by zero")
-
-print("Program continues.")
+    # 잘못된 입력
+    number = int("Not a number")
+    # number = 5 + "Not a number"
+except ValueError:
+    print("Error: Invalid value.")
+except TypeError:
+    print("Error: Invalid type.")
