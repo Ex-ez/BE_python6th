13410003f40b72ba4d9152fe83c0708b373579b0
Younggu Kang <khkkor3@naver.com>
2023-06-19 15:31:22 +0900

    6.19 웹페이지 이미지파일 다운로드 실습

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from urllib.request import urlopen
+import http.client
 from html.parser import HTMLParser
+from pathlib import Path
+from urllib.parse import urljoin, urlunparse
+from urllib.request import urlretrieve
 
 
 class ImageParser(HTMLParser):
@@ -24,16 +27,45 @@ def parse_image(data):
     return data_set
 
 
-def main():
-    url = 'https://google.co.kr'
-    with urlopen(url) as f:
-        charset = f.headers.get_params('charset')[1][1]
-        print(charset)
-        data = f.read().decode(charset)
+def download_image(url, data):
+    download_dir = Path('DOWNLOAD')
+    download_dir.mkdir(exist_ok=True)
 
-    data_set = parse_image(data)
-    print('\n>>>>> Fetch Images from', url)
-    print('\n'.join(sorted(data_set)))
+    parser = ImageParser()
+    parser.feed(data)
+    data_set = set(x for x in parser.result)
+    for x in sorted(data_set):
+        image_url = urljoin(url, x)
+        basename = Path(image_url).name
+        target_file = download_dir / basename
+        print(target_file)
+
+        print("Downloading...", image_url)
+        urlretrieve(image_url, target_file)
+
+
+def main():
+    # url = 'https://google.co.kr'
+    # with urlopen(url) as f:
+    #     charset = f.headers.get_params('charset')[1][1]
+    #     data = f.read().decode(charset)
+    host = "www.google.co.kr"
+    conn = http.client.HTTPConnection(host)
+    conn.request('GET', '')
+    resp = conn.getresponse()
+
+    charset = resp.msg.get_param('charset')
+    print('charset:', charset)
+    data = resp.read().decode(charset)
+    conn.close()
+
+    print('\n>>>>> Downloading...', host)
+    url = urlunparse(('http', host, '', '', '', ''))
+    print('>>>>>', url)
+    # data_set = parse_image(data)
+    # print('\n>>>>> Fetch Images from', url)
+    # print('\n'.join(sorted(data_set)))
+    download_image(url, data)
 
 
 if __name__ == '__main__':
