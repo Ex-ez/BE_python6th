0a5cec38aca31ed715c6921f819f8d6b294f739a
Younggu Kang <khkkor3@naver.com>
2023-06-21 15:15:45 +0900

    6.21 polls vote 함수 구현

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 # Create your views here.
@@ -15,9 +17,20 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def vote(request):
-    pass
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
-def results(request):
-    pass
+def results(request, question_id):
+    return HttpResponseRedirect(reverse('polls:index'))
