174c5f7410c23fbe0a9959a196a65d4b7729782d
Younggu Kang <khkkor3@naver.com>
2023-06-23 14:16:03 +0900

    6.23 프로젝트 홈페이지 추가

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from myproject import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('polls/', include('polls.urls')),
     path('books/', include('books.urls')),
+    path('', views.HomeView.as_view(), name='Home'),
 ]
---
+++ myproject/myproject/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['app_list'] = ['polls', 'books']
+        return context
