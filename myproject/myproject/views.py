3250f77513b8c2a0fdbcf68908066fa25cec097f
Younggu Kang <khkkor3@naver.com>
2023-06-23 14:49:44 +0900

    6.23 apps.pt 변경 실습

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,10 +1,19 @@
+import logging
+
 from django.views.generic import TemplateView
 
+from django.apps import apps
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['app_list'] = ['polls', 'books']
+        # context['app_list'] = ['polls', 'books']
+        dictVerbose = {}
+        for app in apps.get_app_configs():
+            logging.getLogger('TEST').fatal(app.path)
+            if 'site-packages' not in app.path:
+                dictVerbose[app.label] = app.verbose_name
+        context['verbose_dict'] = dictVerbose
         return context
