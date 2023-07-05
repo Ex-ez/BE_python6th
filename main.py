4c47e2aaa3a1ee706a4e127778dea3a45f5e4cc2
Younggu Kang <khkkor3@naver.com>
2023-07-05 13:45:34 +0900

    UPDATE 쿼리실습

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ cur = conn.cursor()
 # ''')
 # conn.commit()
 
+# 데이터 삽입 쿼리
+# cur.execute("INSERT INTO test_table (name) VALUES (%s)", ("Test Name",))
+# conn.commit()
+
+# 데이터 업데이트
+cur.execute("UPDATE test_table SET name = 'Change' WHERE id = 1")
+conn.commit()
+
 cur.execute('SELECT * FROM test_table')
-rows =cur.fetchall()
+rows = cur.fetchall()
 
 for row in rows:
     print(row)
